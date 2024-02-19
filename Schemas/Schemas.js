f6bf3d73179d2bf4719260e3ffadfbee1464daa1
Nourelden Rihan <noureldenrihan@gmail.com>
2024-02-20 00:00:20 +0200

    Comments pt2

--- Schemas/Schemas.js
+++ Schemas/Schemas.js
@@ -2,6 +2,7 @@ const TransactionSchema = require("./Single Schemas/TransactionSchema");
 const BalanceSchema = require("./Single Schemas/BalanceSchema");
 const UserSchema = require("./Single Schemas/UserSchema");
 
+// Schemas Grouper
 const Schemas = {
   UserSchema,
   TransactionSchema,
