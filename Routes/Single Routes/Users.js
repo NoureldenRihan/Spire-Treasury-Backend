19925156b557fcc1027677712555599765dab0ca
Nourelden Rihan <noureldenrihan@gmail.com>
2024-04-14 21:15:14 +0200

    Updated Routes for User Data Retreival to be a POST request instead of GET request

--- Routes/Single Routes/Users.js
+++ Routes/Single Routes/Users.js
@@ -5,6 +5,6 @@ const Controllers = require("../../Controllers/Controllers");
 
 router.post("/signup", Controllers.UsersControllers.createUser);
 router.post("/login", Controllers.UsersControllers.loginUser);
-router.get("/user", Controllers.UsersControllers.getSingleUser);
+router.post("/user", Controllers.UsersControllers.getSingleUser);
 
 module.exports = router;
